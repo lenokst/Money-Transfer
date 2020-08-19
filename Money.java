6890e38ca5598703f47974f5978027094b49463f
Elena Stashevskaya <lenokstash@gmail.com>
2020-08-19 22:07:01 +0300

    add java file

---
+++ Money.java
@@ -0,0 +1,8 @@
+public class Money {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int tranfer = 500_000_000;
+        int totalBalance = balance + tranfer;
+        System.out.println(totalBalance);
+    }
+}
